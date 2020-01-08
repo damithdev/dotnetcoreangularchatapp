d882ca5623618a15430fa17b6506aae347609256
damith <damith.dev@gmail.com>
2020-01-08 16:54:53 +0530

    development

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DatingApp.Dtos
+{
+    public class UserForLoginDto
+    {
+       
+       public string username { get; set; }
+
+       public string password { get; set; }
+    }
+    
+}
---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(12,MinimumLength=8,ErrorMessage = "Password length should be between 8 and 12")]
+        public string Password { get; set; }
+    }
+}
